e33cc564330e2429c1885e0e32037cad8e5a293c
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2015-09-08 19:07:54 -0400

    Ajout de commentaires dans le module Compass.py

--- src/compass.py
+++ src/compass.py
@@ -1,12 +1,28 @@
 from PyQt4.Qwt5 import Qwt
 from PyQt4.Qwt5.Qwt import QwtCompass
 
+##############################################################################
+# 
+# Module Name:            Compass
+# Auteur:                 Benoit Anctil-Robitaille
+# Date:                   8 Septembre 2015
+# Description:            Le module compass.py regroupe les classes necessaire
+#                         a laffichage dun compas. Celui ci affiche la direction
+#                         (Nord-Sud-Est-Ouest)de la fusee durant le vol.
+#
+##############################################################################
+
+#
+# La classe Compass
+# Description:    Classe representant un objet de type QwtCompass
+#
 class Compass(QwtCompass):
-    
+
     def __init__(self, *args):
         QwtCompass.__init__(self, *args)
-        self.rose = Qwt.QwtSimpleCompassRose(16,2)
-        self.rose.setWidth(0.15)
-        self.setRose(self.rose)
-        self.setGeometry(600,410,140,140)
-        self.show()
+        self.rose = Qwt.QwtSimpleCompassRose(16) #Defini le nombre d'aiguille du compass
+        self.rose.setWidth(0.15) #Defini la largeur des aiguilles du compass
+        self.setRose(self.rose) #Initialise la rose du compass
+        self.setGeometry(600,410,140,140) #Positionne le compass
+        self.show() #Affiche le compass
+        
