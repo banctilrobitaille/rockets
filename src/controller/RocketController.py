90e00b756901ecb967ceb8837c2e1036c1588551
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-01-26 18:49:08 -0500

    Added Rocket controller

---
+++ src/controller/RocketController.py
@@ -0,0 +1,60 @@
+from model.Rocket import Rocket
+'''
+Created on 2016-01-26
+
+@author: rockets
+'''
+
+class RocketController(object):
+    
+
+    def __init__(self, rocketModel):
+        
+        self.__rocketModel = Rocket()
+  
+  
+    def updateRocketData(self,speed, altitude, acceleration, temperature, direction, coords, ID, state ):
+        
+        self.updateRocketSpeed(speed)
+        self.updateRocketAltitude(altitude)
+        self.updateRocketAcceleration(acceleration)
+        self.updateRocketTemperature(temperature)
+        self.updateRocketDirection(direction)
+        self.updateRocketCoords(coords)
+        self.updateRocketID(ID)
+        self.updateRocketState(state)
+        
+    
+    def updateRocketSpeed(self,speed):
+        
+        self.__rocketModel.speed(speed)
+    
+    
+    def updateRocketAltitude(self,altitude):
+        
+        self.__rocketModel.altitude(altitude)
+    
+    
+    def updateRocketAcceleration(self,acceleration):
+        
+        self.__rocketModel.acceleration(acceleration)
+    
+    
+    def updateRocketTemperature(self,temperature):
+        
+        self.__rocketModel.temperature(temperature)
+    
+    def updateRocketDirection(self,direction):
+        
+        self.__rocketModel.direction(direction)
+    
+    def updateRocketCoords(self,coords):
+        
+        
+    
+    def updateRocketID(self, ID):
+        pass
+    
+    def updateRocketState(self,state):
+        pass
+    
