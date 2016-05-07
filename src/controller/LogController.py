79831b8f2564c4b44d82f93c313333225cd7ae6f
aminew7 <amine.waddah@gmail.com>
2016-05-07 15:28:58 -0400

    XML file class. Read, Write and startFlight methods

--- src/controller/LogController.py
+++ src/controller/LogController.py
@@ -15,55 +15,101 @@ class LogController(object):
 
 
     def startFlight(self, rocketID, startTime):
+
+        #inflight = False
+        #if (state == '2'):
+            #print('in flight, need to terminate the flight')
+            #inflight = True
+
         pass
+
 
     def endFlight(self, endTime):
+
         pass
 
+
     def addData(self, timestamp, state, speed, altitude,acceleration, latitude, longitude, temperature, crc):
-        root=Element('Flight')
+
+        root=Element('FlightData')
         tree=ElementTree(root)
-        rocketid=Element('RocketID')
-        root.append(rocketid)
-        ts = timestamp
-        #st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        timestamp = ET.SubElement(rocketid, 'timestamp')
-        #timestamp.text = '39432949324'
-        timestamp.text = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        state=ET.SubElement(rocketid,'state')
-        #state.text = '0'
+
+        date=Element('Date')
+        root.append(date)
+
+        ts = time.time()
+        date.set('date', datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+        rocketid=ET.SubElement(date,'RocketID')
+        rocketid.set('id', id)
+
+        timestamp = ET.SubElement(date, 'timestamp')
+        timestamp.text = timestamp
+
+        state=ET.SubElement(date,'state')
         state.text = state
-        speed=ET.SubElement(rocketid,'speed')
-        #speed.text = '450'
+        #state.text = '0'
+
+
+        speed=ET.SubElement(date,'speed')
         speed.text = speed
-        altitude=ET.SubElement(rocketid,'altitude')
-        #altitude.text = '10000'
+        #speed.text = '450'
+
+
+        altitude=ET.SubElement(date,'altitude')
         altitude.text = altitude
-        acceleration=ET.SubElement(rocketid,'acceleration')
-        #acceleration.text = '45'
+        #altitude.text = '10000'
+
+
+        acceleration=ET.SubElement(date,'acceleration')
         acceleration.text = acceleration
-        latitude = ET.SubElement(rocketid,'latitude')
-        #latitude.text = '37.77184'
+        #acceleration.text = '45'
+
+        latitude = ET.SubElement(date,'latitude')
         latitude.text = latitude
-        longitude=ET.SubElement(rocketid,'longitude')
+        #latitude.text = '37.77184'
+
+
+        longitude = ET.SubElement(date,'longitude')
         longitude.text = longitude
         #longitude.text = '11.500'
-        temperature=ET.SubElement(rocketid,'temperature')
-        #temperature.text = '20'
+
+        temperature = ET.SubElement(date,'temperature')
         temperature.text = temperature
-        crc=ET.SubElement(rocketid, 'crc')
+        #temperature.text = '20'
+
+        crc=ET.SubElement(date, 'crc')
         crc.text = crc
         #crc.text = '200'
-        root.set('date', datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-        rocketid.set('id', id)
-        print ET.tostring(root)
-        tree.write(open(r'/home/rockets/flight.xml','w'))
-        #ts = time.time()
-        #print ts
-        #st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        #print st
-    
-   
-       
 
-        
+
+        print ET.tostring(root)
+        #tree.write(open(r'/home/rockets/flight.xml','w'))
+
+
+
+class XMLFile(object):
+
+
+    def writeFile(self, file):
+
+        ET.write(file)
+
+
+    def readFile(file):
+
+        inflight = False
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        for Date in root.findall('Date'):
+            date = Date.get('date')
+            state = Date.find('state').text
+            print 'date : ' + date, 'state: ' + state
+
+        if state == '2':
+            print('in flight, need to stop the flight')
+
+
+
+
